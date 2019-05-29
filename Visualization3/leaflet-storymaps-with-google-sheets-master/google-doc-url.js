04df93e24f45eee692ed2976a8b9074e64c35f6a
GroupText <50390890+GroupText@users.noreply.github.com>
2019-05-29 12:44:11 -0700

    Update google-doc-url.js

--- Visualization3/leaflet-storymaps-with-google-sheets-master/google-doc-url.js
+++ Visualization3/leaflet-storymaps-with-google-sheets-master/google-doc-url.js
@@ -1,1 +1,2 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1pL6mCdF9T2_FuFFwTMJ6x4PGYqmHJ7dyHdjQIbJNw5k/edit#gid=0';
+// var googleDocURL = 'https://docs.google.com/spreadsheets/d/1pL6mCdF9T2_FuFFwTMJ6x4PGYqmHJ7dyHdjQIbJNw5k/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Y9dG_mlwN0m4DGYTHq6DOqH_VF05hwxbbIDExjK3dm4/edit#gid=0';
