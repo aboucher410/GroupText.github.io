6900812ebbff99e783946e5aa0a5836064f7eebe
GroupText <50390890+GroupText@users.noreply.github.com>
2019-05-28 18:16:38 -0700

    Update map.js

--- Visualization1/map.js
+++ Visualization1/map.js
@@ -20,31 +20,31 @@ var combined = L.geoJson(combined_spaces, {style: {stroke: false, fillColor: "wh
 
 //green space
 $.getJSON("CAMPUS_GEOJSONS/CAMPUS_GEOJSON_LANDSCAPE.geojson",function(data){
-    var greenspace = L.geoJson(data, {style: {stroke: false, fillColor: "black", fillOpacity: .3}}).addTo(map);
+    var greenspace = L.geoJson(data, {style: {stroke: false, fillColor: "black", fillOpacity: .5}}).addTo(map);
     controlLayer.addOverlay(greenspace, "Green Space");
 });
 
 //diversity space
 $.getJSON("CAMPUS_GEOJSONS/CAMPUS_GEOJSON_DIVERSITY.geojson",function(data){
-    var diversity = L.geoJson(data, {style: {stroke: false, fillColor: "purple", fillOpacity: .3}}).addTo(map);
+    var diversity = L.geoJson(data, {style: {stroke: false, fillColor: "purple", fillOpacity: .5}}).addTo(map);
     controlLayer.addOverlay(diversity, "Diversity Space");
 });
 
 //social space
 $.getJSON("CAMPUS_GEOJSONS/CAMPUS_GEOJSON_SOCIAL.geojson",function(data){
-    var social = L.geoJson(data, {style: {stroke: false, fillColor: "green", fillOpacity: .3}}).addTo(map);
+    var social = L.geoJson(data, {style: {stroke: false, fillColor: "green", fillOpacity: .5}}).addTo(map);
     controlLayer.addOverlay(social, "Social Space");
 });
 
 //study space
 $.getJSON("CAMPUS_GEOJSONS/CAMPUS_GEOJSON_STUDY.geojson",function(data){
-    var study = L.geoJson(data, {style: {stroke: false, fillColor: "#FF0000", fillOpacity: .3}}).addTo(map); //red
+    var study = L.geoJson(data, {style: {stroke: false, fillColor: "#FF0000", fillOpacity: .5}}).addTo(map); //red
     controlLayer.addOverlay(study, "Study Space");
 });
 
 //path space
 $.getJSON("CAMPUS_GEOJSONS/CAMPUS_GEOJSON_PATHS.geojson",function(data){
-    var paths = L.geoJson(data, {style: {stroke: false, fillColor: "yellow", fillOpacity: .3}}).addTo(map);
+    var paths = L.geoJson(data, {style: {stroke: false, fillColor: "yellow", fillOpacity: .5}}).addTo(map);
     controlLayer.addOverlay(paths, "ADA Connecting Space");
 });
 
