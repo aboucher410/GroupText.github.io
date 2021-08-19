f5a01c630d482997ffd0bc0f763b496b876c2616
aboucher410 <46583858+aboucher410@users.noreply.github.com>
2021-08-19 10:02:29 -0700

    Update google-doc-url.js

--- Visualization3/leaflet-storymaps-with-google-sheets-master/google-doc-url.js
+++ Visualization3/leaflet-storymaps-with-google-sheets-master/google-doc-url.js
@@ -1,2 +1,2 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1pL6mCdF9T2_FuFFwTMJ6x4PGYqmHJ7dyHdjQIbJNw5k/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1EQC_gPt-craVQ9QfjAHZvGAEVZUyieIVBYAKRYNwn2c/edit?usp=sharing';
 
